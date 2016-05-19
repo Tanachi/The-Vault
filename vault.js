464912202f84b3e89e7e5ff6c023873d72df3f1b
Tanachi <tanachige@gmail.com>
2016-05-18 19:12:12 -1000

    Added getValue

--- vault.js
+++ vault.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+  var keyVault = '';
+  var valueVault = '';
+  return {
+  getValue:function(key){
+    if(!key)
+      return null;
+    if(key === keyVault)
+      return valueVault;
+    else
+      return null;
+  }
+  };
 };
